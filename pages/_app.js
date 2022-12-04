e696fa7f9e1e7c5962f89ecd5dd1f01b5bb1d2a2
epicprogramming101 <46630586+ternarysrche@users.noreply.github.com>
2022-12-04 00:56:01 -0800

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/global.css';
 import { useRouter } from 'next/router';
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }) {
   
-    return <Component {...pageProps} />;
+    return (<><Component {...pageProps} /><Analytics /></>);
 }
